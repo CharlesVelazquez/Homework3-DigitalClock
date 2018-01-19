89518dfaf40d978f6e25f60ed545567613f21cee
Charles Velazquez <CharlesVelazquez15@gmail.com>
2018-01-19 15:25:48 -0500

    almost done =)

--- index.js
+++ index.js
@@ -1,68 +1,7 @@
-//declaring elements
-// var clock = document.getElementById('clock');//Grabbing canvas
-// var context = clock.getContext ('2d');//grabbing internal stuff
-// var radius = clock.height / 2; //self explanitory
-// context.translate (radius, radius);//moves context to the center of the object
-// radius = radius * 0.90;//necessary to properly draw clock by reducing the radius to 90%
-// drawClock();
-
-// function drawClock()//does the actual drawing of the clock
-// {
-// 	context.arc (0,0,radius,0,2 * Math.PI);
-// 	context.fillStyle = 'White';
-// 	context.fill();
-// 	drawFace (context, radius); //makes the clock face minus the hands
-// 	makeNumbers (context, radius);
-// }
-
-// function drawFace(context, radius)
-// {
-// 	var gradient; //going to use to add that gradient effect
-// 	context.beginPath();//starting a drawing location
-// 	context.arc(0, 0, radius, 0, 2*Math.PI);//draws a circle for me =)
-//     context.fillStyle = 'white';//Da color
-//     context.fill();//Actually executes the drawing
-
-//     gradient = context.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);//this I learned from w3schools, I still don't really understand it
-//     gradient.addColorStop(0, '#333');//color effect for inner
-//     gradient.addColorStop(0.5, 'white');//color effect for middle
-//     gradient.addColorStop(1, '#333');//color effect for outer
-//     context.strokeStyle = gradient; //the gradient is now like A brush that will be used to draw =)
-//     context.lineWidth = radius*0.1; //making a line 10% of the radius
-//     context.stroke();//actually draw
-
-//     context.beginPath();//setting a new location, drawing the clock nose below
-//     context.arc(0, 0, radius*0.1, 0, 2*Math.PI);
-//     context.fillStyle = '#333';
-//     context.fill();
-// }
-
-// function makeNumbers (context, radius)
-// {
-// 	var angle //will dictate location
-// 	var number //the actual numbers
-// 	ctx.font = radius*0.15 + "px arial";
-// }
-
-
-// context.beginPath();
-// context.arc(95,50,40,0,2*Math.PI);
-// context.stroke();
-
-//.createLinearGradient(x,y,x1,y1) - creates a linear gradient
-//.createRadialGradient(x,y,r,x1,y1,r1) - creates a radial/circular gradient
-//.addColorStop() method specifies the color stops, and its position along the gradient. Gradient positions can be anywhere between 0 to 1
-//*Makes the blur ^
-
-//font - defines the font properties for the text
-//fillText(text,x,y) - draws "filled" text on the canvas
-//strokeText(text,x,y) - draws text on the canvas (no fill)
-
 var time = new Date(); //Grabs the Time
 var hour = document.getElementById('hour')//Targets place hours will go
 var minute = document.getElementById('minute')//Targets place minutes will go
 var second = document.getElementById('second')//Targets place seconds will go
-var background = document.getElementById('background')//Targets place the background picture will go
 var greeting = document.getElementById('greeting')
 var check_hour, check_minute, check_second;// declared variables I'll use to check the corrosponding time
 
@@ -76,9 +15,11 @@ setInterval (function ()
 //These if statments add a 0 if the numbers are single digits
 if (check_hour < 10)
     {hour.innerHTML = '0' + time.getHours() + ':';}
-else if (check_hour >= 13 && check_hour <= 23)
-    {hour.innerHTML = time.getHours() - 12 + ':';}
-else if (check_hour == 0)
+
+else if (check_hour >= 13 && check_hour <= 23){
+        var fix0 = time.getHours() - 12
+        hour.innerHTML = '0' + fix0 + ':';
+}else if (check_hour == 0)
     {hour.innerHTML = time.getHours() + '1:';}
 else
     {hour.innerHTML = time.getHours() + ':';}
@@ -100,27 +41,30 @@ if (check_hour < 12)//Morning
     {
         document.body.style.backgroundImage = "url('img/Morning.png')"
         document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundRepeat = "repeat-y";
+        document.body.style.backgroundRepeat = "no-repeat";
+        greeting.innerHTML = 'Good Morning';
     }
 else if (check_hour >= 12 && check_hour <= 16)//Aftermoon
     {
         document.body.style.backgroundImage = "url('img/Afternoon.jpg')"
         document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundRepeat = "repeat-y";
+        document.body.style.backgroundRepeat = "no-repeat";
         greeting.innerHTML = 'Good Afternoon';
     }
 else if (check_hour >= 17 && check_hour <= 19)//Evening
     {
         document.body.style.backgroundImage = "url('img/Evening.jpg')"
         document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundRepeat = "repeat-y";
+        document.body.style.backgroundRepeat = "no-repeat";
+        greeting.innerHTML = 'Good Evening';
     }
 
     if (check_hour >= 20 && check_hour <= 23)//Night
     {
         document.body.style.backgroundImage = "url('img/Night.jpg')"
         document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundRepeat = "repeat-y";
+        document.body.style.backgroundRepeat = "no-repeat";
+        greeting.innerHTML = 'Good Night';
     }
 
 }, 1000)
