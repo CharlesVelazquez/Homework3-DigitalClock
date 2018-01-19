8059b64381f772d96720a51c9fa0e1cc16b53e03
Charles Velazquez <CharlesVelazquez15@gmail.com>
2018-01-19 13:10:14 -0500

    Finished background, working greeting

--- index.js
+++ index.js
@@ -59,19 +59,20 @@
 //strokeText(text,x,y) - draws text on the canvas (no fill)
 
 var time = new Date(); //Grabs the Time
-var hour = document.getElementById('hour')
-var minute = document.getElementById('minute')
-var second = document.getElementById('second')
-var background = document.getElementById('Background')
-var check_hour, check_minute, check_second;
+var hour = document.getElementById('hour')//Targets place hours will go
+var minute = document.getElementById('minute')//Targets place minutes will go
+var second = document.getElementById('second')//Targets place seconds will go
+var background = document.getElementById('background')//Targets place the background picture will go
+var check_hour, check_minute, check_second;// declared variables I'll use to check the corrosponding time
 
+//Function to set the time in the HTML and Background
 setInterval (function ()
 {
     time = new Date();
     check_hour = time.getHours();
     check_minute = time.getMinutes();
     check_second = time.getSeconds();
-    
+//These if statments add a 0 if the numbers are single digits
 if (check_hour < 10)
     {hour.innerHTML = '0' + time.getHours() + ':';}
 else if (check_hour >= 13 && check_hour <= 23)
@@ -79,20 +80,50 @@ else if (check_hour >= 13 && check_hour <= 23)
 else if (check_hour == 0)
     {hour.innerHTML = time.getHours() + '1:';}
 else
-    {hour.innerHTML = time.getHours() + 1 + ':';}
+    {hour.innerHTML = time.getHours() + ':';}
 
 if (check_minute < 10)
     {minute.innerHTML = '0' + time.getMinutes() + ':';}
 else
     {minute.innerHTML = time.getMinutes() + ':';}
 
+//Also added a function to seconds to add the AM and PM
 if (check_second < 10)
     {second.innerHTML = '0' + time.getSeconds() + am_pm(check_hour);}
 else
     {second.innerHTML = time.getSeconds() + am_pm(check_hour);}
 
+//Now making the if statments to change background and greeting
+
+if (check_hour < 12)//Morning
+    {
+        document.body.style.backgroundImage = "url('img/Morning.png')"
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundRepeat = "repeat-y";
+    }
+else if (check_hour >= 12 && check_hour <= 16)//Aftermoon
+    {
+        document.body.style.backgroundImage = "url('img/Afternoon.jpg')"
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundRepeat = "repeat-y";
+    }
+else if (check_hour >= 17 && check_hour <= 19)//Evening
+    {
+        document.body.style.backgroundImage = "url('img/Evening.jpg')"
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundRepeat = "repeat-y";
+    }
+
+    if (check_hour >= 20 && check_hour <= 23)//Night
+    {
+        document.body.style.backgroundImage = "url('img/Night.jpg')"
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundRepeat = "repeat-y";
+    }
+
 }, 1000)
 
+//Function to return AM or PM when invoked
 function am_pm (check_hour)
 {
     if (check_hour >=12)
@@ -103,5 +134,4 @@ function am_pm (check_hour)
         {
             return ' AM'
         }
-    // body...
 }
